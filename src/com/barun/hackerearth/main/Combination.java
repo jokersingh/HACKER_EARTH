fe201971b32e756e39905d7795f1274e8906a306
jokersingh <sakhibarun@gmail.com>
2014-08-18 13:01:18 +0530

    Combination

---
+++ src/com/barun/hackerearth/main/Combination.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.barun.hackerearth.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bbarun
+ *
+ */
+public class Combination {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String name = "barun";
+		char[] nameArray = name.toCharArray();
+		int i=1;
+		for(char ch:nameArray){
+			List<String> combination = combinations(ch,i);
+			for(String str:combination){
+				System.out.println(str);
+			}
+		}
+
+	}
+
+	private static List<String> combinations(char ch, int i) {
+		List<String> combinations = new ArrayList<String>();
+		
+		return null;
+	}
+
+}
